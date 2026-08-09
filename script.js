8f8b364abd164ac50c8e71e78325a02bf8b625f4
Supriya Shivakumar <supriya300714@gmail.com>
2026-08-09 17:34:04 +0530

    sketch with black color works

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 const container = document.querySelector(".grid-container");
-const gridSize = 16;
-const resetButton = document.querySelector(".reset");
-const square = document.querySelector("div");
 
-function createGrid () {
+function createGrid(gridSize) {
     for (let i = 0; i < gridSize * gridSize; i++) {
-        const gridCell = document.createElement('div');
-        gridCell.classList.add('grid-cell');
-        container.insertAdjacentElement('beforeend', gridCell);
+        const gridCell = document.createElement("div");
+        gridCell.classList.add("grid-cell");
+
+        gridCell.addEventListener("mouseover", function(event) {
+            event.target.classList.add("color");
+        });
+
+        container.appendChild(gridCell);
     }
 }
 
-square.addEventListener("mouseover", function(event) {
-    event.target.classList.replace("square", ".color");
-});
-
-resetButton.addEventListener("click", function () {
-    container.innerHTML = "";
-    createGrid();
-});
-
-createGrid();
+createGrid(16);
