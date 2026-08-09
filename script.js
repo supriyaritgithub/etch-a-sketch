ab2195fe3c187416df24d96c7f628c5ce8379699
Supriya Shivakumar <supriya300714@gmail.com>
2026-08-09 16:41:15 +0530

    functions added

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const container = document.querySelector('.grid-container');
+const container = document.querySelector(".grid-container");
 const gridSize = 16;
+const resetButton = document.querySelector(".reset");
+const square = document.querySelector("div");
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-    const gridCell = document.createElement('div');
-    gridCell.classList.add('grid-cell');
-    container.insertAdjacentElement('beforeend', gridCell);
+function createGrid () {
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('grid-cell');
+        container.insertAdjacentElement('beforeend', gridCell);
+    }
 }
+
+square.addEventListener("mouseover", function(event) {
+    event.target.classList.replace("square", ".color");
+});
+
+resetButton.addEventListener("click", function () {
+    container.innerHTML = "";
+    createGrid();
+});
+
+createGrid();
